c438dc70bd0683f25d01a21f97850f508db83f8f
Alex Salman <aalsalma@ucsc.edu>
2021-01-10 13:32:14 -0800

    Interpreter

--- arith.py
+++ arith.py
@@ -23,10 +23,15 @@ class Parser(object):
     def __init__(self, input):
         self.input = input
         self.current = self.input.get_next_token()
+# error catcher
+    def syntax_error(self):
+        raise Exception('you have a syntax error')
 # comapre token type
     def compare_str(self, token_type):
         if self.current.type == token_type:
             self.current = self.input.get_next_token()
+        else:
+            self.syntax_error()
 # check if value or expression
     def value_or_expression(self):
         token = self.current
@@ -59,7 +64,25 @@ class Parser(object):
             elif token.type = mns:
                 self.compare_str(mns)
             node = Binary_Operation(left=node, operation=token, right=self.div_mlt())
+        return node
+# parse pls_mns
+    def parse_pls_mns(self):
+        return self.pls_mns()
 ################################################################################
-# interpreter
-
+# interpreter for tree traversal, AST
+class Interpreter(node):
+# constructor
+    def __init__(self, parse_pls_mns):
+        self.parse_pls_mns = parse_pls_mns
+# check if children equels either of the arithmatic operations
+    def interpret_Binary_Operation(self, node):
+        if node.operation.type == pls:
+            return self.node.left + self.node.right
+        elif node.operation.type == mns:
+            return self.node.left - self.node.right
+        elif node.operation.type == div:
+            return self.node.left / self.node.right
+        elif node.operation.type == mlt:
+            return self.node.left * self.node.right
+    
 # test cases
